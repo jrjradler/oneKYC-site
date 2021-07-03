9c38cfa2cd799562298231b7300abd432481db4e
jrjradler <adlerrjake@gmail.com>
2021-07-03 07:27:39 +0400

    Temp updates 2.0

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 import Header from '../partials/Header';
 import HeroHome from '../partials/HeroHome';
-import FeaturesHome from '../partials/Features';
+{/* import FeaturesHome from '../partials/Features'; */}
 import Footer from '../partials/Footer';
 
 function Home() {
--- src/partials/HeroHome.js
+++ src/partials/HeroHome.js
@@ -39,9 +39,10 @@ function HeroHome() {
                 <div>
                   <a className="btn text-white bg-blue-600 hover:bg-blue-700 w-full mb-4 sm:w-auto sm:mb-0" href="/signup">Request Access</a>
                 </div>
+                {/* Learn more button 
                 <div>
                   <a className="btn text-white bg-gray-900 hover:bg-gray-800 w-full sm:w-auto sm:ml-4" href="#0">Learn more</a>
-                </div>
+                </div> */}
               </div>
             </div>
           </div>
